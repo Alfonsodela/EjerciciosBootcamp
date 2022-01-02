11de2cfaf98bdbfc4e3453bee846ec02f584de28
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-01-02 12:22:00 +0100

    add JS

---
+++ Ejercicios_JS/Advanced/JS/01-DOM.js
@@ -0,0 +1,47 @@
+window.onload = () => {
+
+  // 1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
+
+    const showMe = document.querySelector('.showme');
+    console.log(showMe);
+    
+
+
+  // 1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
+
+  const pillado = document.querySelector('#pillado');
+  console.log(pillado);
+
+
+
+  // 1.3 Usa querySelector para mostrar por consola todos los p.
+
+  const allP = document.querySelectorAll('p');
+  allP[0].style.color = 'blue';
+  console.log(allP);
+
+  
+
+  // 1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
+
+  const pokemon = document.querySelectorAll('.pokemon')
+  console.log(pokemon);
+
+
+
+  // 1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo
+  // data-function="testMe".
+
+  const dataFunction = document.querySelectorAll('[data-function="testMe"]');
+  console.log(dataFunction);
+
+
+
+  // 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo
+  // data-function="testMe".
+
+  const rick = document.querySelectorAll('[data-function="testMe"]')
+  console.log(rick[2]);
+
+    
+};
---
+++ Ejercicios_JS/Advanced/JS/02-DOM.js
@@ -0,0 +1,92 @@
+window.onload = () => {
+
+  // 2.1 Inserta dinamicamente en un html un div vacio con javascript.
+
+    const newDiv = document.createElement('newDiv');
+    const emptyDiv = document.getElementById('prueba');
+    emptyDiv.appendChild(newDiv);
+    // console.log(emptyDiv);
+    
+
+
+  // 2.2 Inserta dinamicamente en un html un div que contenga una p con javascript.
+
+    const p = document.createElement('p');
+    emptyDiv.appendChild(p);
+    // console.log(emptyDiv);
+
+
+
+  // 2.3 Inserta dinamicamente en un html un div que contenga 6 p utilizando un loop con javascript.
+
+    for (let i = 0; i < 6; i++) {
+        const p6 = document.createElement('p');
+        emptyDiv.appendChild(p6)
+        // console.log(emptyDiv);        
+    }
+
+
+
+  // 2.4 Inserta dinamicamente con javascript en un html una p con el texto 'Soy dinámico!'.
+
+    const soyDinamico = document.createElement('p')
+    emptyDiv.appendChild(soyDinamico);
+    soyDinamico.textContent = 'Soy dinámico';
+    // console.log(emptyDiv);
+
+
+
+  // 2.5 Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
+
+    const wubba = document.querySelector('.fn-insert-here');
+    wubba.textContent = 'Wubba Lubba dub dub';
+    // console.log(document.querySelector('.fn-insert-here'));
+
+
+
+  // 2.6 Basandote en el siguiente array crea una lista ul > li con los textos del array.
+    const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+
+    const ul = document.createElement('ul');
+    emptyDiv.appendChild(ul);
+
+    apps.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = item;
+        ul.appendChild(li)
+    });
+
+    // console.log(emptyDiv);
+
+
+
+  // 2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
+
+    const eliminame = document.querySelectorAll('.fn-remove-me')
+
+    eliminame.forEach(item => {
+        item.remove()
+    })
+
+
+
+  // 2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div.
+  // Recuerda que no solo puedes insertar elementos con .appendChild.
+
+    const pDiv = document.createElement('pDiv');
+    const div = document.querySelectorAll('div');
+    // div.appendChild(pDiv)
+    document.body.insertBefore(pDiv, div[1]);
+    pDiv.textContent = 'Voy en medio';
+    console.log(div)
+  
+
+    
+  // 2.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase .fn-insert-here
+  // const div = document.querySelectorAll("div.fn-insert-here")
+  // div.forEach(item => {
+  // const p = document.createElement('p')
+  // p.textContent = 'Voy dentro!'
+  // item.appendChild(p)
+  // })
+};
