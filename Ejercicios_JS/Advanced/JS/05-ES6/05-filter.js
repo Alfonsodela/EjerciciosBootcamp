fab6ccf76493c69f3d8d42eec64af4af26ee338a
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-03-18 21:27:42 +0100

    filter

---
+++ Ejercicios_JS/Advanced/JS/05-ES6/05-filter.js
@@ -0,0 +1,14 @@
+// 5.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los valores que sean mayor que 18
+// const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+// const higher = ages.filter(item => {
+// return item > 18
+// })
+// console.log(higher)
+// 5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los valores que sean par.
+// const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+// const par = ages.filter(item => {
+// return item % 2 === 0
+// })
+// console.log(par)
