bcf718d980523f93d68e73509d851e7e27a9e543
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-04-23 12:59:50 +0200

    add

--- Ejercicios_JS/Advanced/JS/05-ES6/06-find.js
+++ Ejercicios_JS/Advanced/JS/05-ES6/06-find.js
@@ -1,9 +1,3 @@
-// 6.1 Dado el siguiente array, usa .find() para econtrar el núme
-// const numbers = [32, 21, 63, 95, 100, 67, 43];
-// 
-// const res = numbers.find(item => {
-// return item === 100
-// })
-// console.log(res)
+
 
 
