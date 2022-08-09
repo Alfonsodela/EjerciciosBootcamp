87ff143225c2960df2306ce057f205a1b5d2c573
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-08-09 13:03:45 +0200

    variables

--- Ejercicios_JS/Basics/JS_Basics/01-variables.js
+++ Ejercicios_JS/Basics/JS_Basics/01-variables.js
@@ -63,14 +63,13 @@ console.log(z)
 // let globalBasePrice = 10000;
 // let car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 // let car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000}
-// // 
+// // // 
 // globalBasePrice = 25000;
 
+// car1 = car1.basePrice + globalBasePrice
+// console.log(car1);
 
-// car1 = car1.basePrice + globalBasePrice;
-// console.log(car1)
-// 
-// car2 = car2.basePrice + globalBasePrice;
-// console.log(car2)
+// car2 = car2.basePrice + globalBasePrice
+// console.log(car2);
 
-};
+}
--- Ejercicios_JS/Basics/Loops/01-loops.js
+++ Ejercicios_JS/Basics/Loops/01-loops.js
@@ -1,4 +1,4 @@
-// window.onload = () => {
+window.onload = () => {
 
     // Iteración #1: Usa includes
 
@@ -8,7 +8,10 @@
     // const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket',
     // 'Cinrurón de Orión', 'AC/DC Camiseta']
 
-    
+        // products.forEach(item => {
+    //     if (item.includes('Camiseta'))
+    //     console.log(item)
+    // })
 
 
     // Iteración #2: Condicionales avanzados
@@ -19,15 +22,30 @@
 
 
     // const alumns = [
-        // { name: 'Pepe Viruela', T1: false, T2: false, T3: true },
-        // { name: 'Lucia Aranda', T1: true, T2: false, T3: true },
-        // { name: 'Juan Miranda', T1: false, T2: true, T3: true },
-        // { name: 'Alfredo Blanco', T1: false, T2: false, T3: false },
-        // { name: 'Raquel Benito', T1: true, T2: true, T3: true }
+    //     { name: 'Pepe Viruela', T1: false, T2: false, T3: true },
+    //     { name: 'Lucia Aranda', T1: true, T2: false, T3: true },
+    //     { name: 'Juan Miranda', T1: false, T2: true, T3: true },
+    //     { name: 'Alfredo Blanco', T1: false, T2: false, T3: false },
+    //     { name: 'Raquel Benito', T1: true, T2: true, T3: true }
     // ]
 
     
+    // alumns.forEach(item => {
+    //    if ((item.T1 + item.T2 + item.T3)>=2) {
+    //     item.isApproved = true
+    //    } else {
+    //     item.isApproved = false
+    //    }
+    //    console.log(item)
+    // })
 
+    // alumns.forEach(alumno => {
+
+    //     ((alumno.T1 + alumno.T2 + alumno.T3)>= 2) ? alumno.isApproved = true : alumno.isApproved = false;
+    //     console.log(alumno)
+    // });
+    
+    
 
     // Iteración #3: Probando For...of
 
@@ -37,9 +55,10 @@
 
     // const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
 
+    // for ( const place of placesToTravel) {
+        // console.log(place)
+    // }
     
-
-
 
     // Iteración #4: Probando For...in
 
@@ -53,23 +72,28 @@
     // weight: '259kg'
     // }
 
-   
+    // for (const properties in alien) {
+    //     console.log(alien[properties])
+    // }
 
-
-
+    
     // Iteración #5: Probando For
 
     // Usa un bucle **for** para recorrer todos los destinos del array y elimina 
     // los elementos que tengan el id 11 y 40. Imprime en un ***console log*** el array. 
     // Puedes usar este array:
 
-// 
+// // 
     // const placesToTravel = [{ id: 5, name: 'Japan' }, { id: 11, name: 'Venecia' },
     // { id: 23, name: 'Murcia' }, { id: 40, name: 'Santander' }, { id: 44, name: 'Filipinas' },
     // { id: 59, name: 'Madagascar' }]
-// 
-    
 
+   //     placesToTravel.forEach(place => {
+//         if (place.id == 11 || place.id == 40) {
+//             delete place  
+//             console.log(place)        
+//         }
+//     })
 
 
     //Iteración #6: Mixed For...of e includes
@@ -79,14 +103,19 @@
     //función ***.includes()*** para comprobarlo.Puedes usar este array:
 
     // const toys = [
-        // { id: 5, name: 'Buzz MyYear' },
-        // { id: 11, name: 'Action Woman' },
-        // { id: 23, name: 'Barbie Man' },
-        // { id: 40, name: 'El gato con Guantes' },
-        // { id: 40, name: 'El gato felix' }
+    //     { id: 5, name: 'Buzz MyYear' },
+    //     { id: 11, name: 'Action Woman' },
+    //     { id: 23, name: 'Barbie Man' },
+    //     { id: 40, name: 'El gato con Guantes' },
+    //     { id: 40, name: 'El gato felix' }
     // ]
 
-    
+        // for ( const toy of toys) {
+    //     if (toy.name.includes('gato')) {
+    //         toys.splice(toys.indexOf(toy))           
+    //     }
+    //     console.log(toys)
+    // }
 
 
 // 
@@ -96,15 +125,22 @@
     // tengan más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola.. 
     // Puedes usar este array:
 
-    // const popularToys = [];
-    // 
-    // const toys = [
-        // { id: 5, name: 'Buzz MyYear', sellCount: 10 },
-        // { id: 11, name: 'Action Woman', sellCount: 24 },
-        // { id: 23, name: 'Barbie Man', sellCount: 15 },
-        // { id: 40, name: 'El gato con Guantes', sellCount: 8 },
-        // { id: 40, name: 'El gato felix', sellCount: 35 }
-    // ]
+    const popularToys = [];
+    
+    const toys = [
+        { id: 5, name: 'Buzz MyYear', sellCount: 10 },
+        { id: 11, name: 'Action Woman', sellCount: 24 },
+        { id: 23, name: 'Barbie Man', sellCount: 15 },
+        { id: 40, name: 'El gato con Guantes', sellCount: 8 },
+        { id: 40, name: 'El gato felix', sellCount: 35 }
+    ]
+
+    for (const toy of toys) {
+       if (toy.sellCount > 15) {
+        popularToys.push(toy)
+       }
+       console.log(popularToys)
+    }
 
    
-// }
+}
